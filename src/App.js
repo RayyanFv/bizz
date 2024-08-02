9c16a09abde090a2c4d626108c08ef29d9ddf956
RayyanFv <rayyanfv@gmail.com>
2024-08-03 01:21:41 +0700

    Add routing fallback to homepage and vercel.json configuration

--- src/App.js
+++ src/App.js
@@ -4,34 +4,38 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
-import Digimarket from './pages/Digimarket';
-import BiztrackMonitor from './pages/BiztrackMonitor';
-import BizliveInsight from './pages/BizliveInsight';
-import SkillboostPro from './pages/SkillboostPro';
-import BizzguidePro from './pages/BizguidePro';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import '../src/Styles/style.css';
+import BiztrackMonitor from './pages/BiztrackMonitor';
+import Digimarket from './pages/Digimarket';
+import BizliveInsight from './pages/BizliveInsight';
+import BizguidePro from './pages/BizguidePro';
+import SkillboostPro from './pages/SkillboostPro';
+// import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute if you have
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="content">
+      <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/digimarket" element={<Digimarket />} />
-          <Route path="/biztrack-monitor" element={<BiztrackMonitor />} />
-          <Route path="/bizlive-insight" element={<BizliveInsight />} />
-          <Route path="/skillboost-pro" element={<SkillboostPro />} />
-          <Route path="/bizzguide-pro" element={<BizzguidePro />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/biztrack-monitor" element={<BiztrackMonitor />} />
+            <Route path="/digimarket" element={<Digimarket />} />
+            <Route path="/bizlive-insight" element={<BizliveInsight />} />
+            <Route path="/bizguide-pro" element={<BizguidePro />} />
+            <Route path="/skillboost-pro" element={<SkillboostPro />} />
+          </Route>
+          {/* Tambahkan rute fallback */}
+          <Route path="*" element={<Homepage />} />
         </Routes>
+        <Footer />
       </div>
-      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
